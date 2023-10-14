f808d117d39fa47b2b8dc36f681db17611e7da24
MojMasala <mojmasala07@gmail.com>
2023-10-14 11:16:04 +0100

    first commit

---
+++ src/test/java/org/example/SeleniumBasic/OpenUrl.java
@@ -0,0 +1,15 @@
+package org.example.SeleniumBasic;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenUrl {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.next.co.uk/");
+    }
+}
