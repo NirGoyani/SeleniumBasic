bf24fbea1a1bee9770feedd35efea7386b4df0a8
MojMasala <mojmasala07@gmail.com>
2023-10-14 11:39:46 +0100

    I have added Login Functionality

---
+++ src/test/java/org/example/SeleniumBasic/LoginFunctionality.java
@@ -0,0 +1,20 @@
+package org.example.SeleniumBasic;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginFunctionality {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.next.co.uk/");
+        //....
+        //...
+        // Login Functinality test
+
+        //commit -- code / button
+        // push -- code / button
+    }
+}
