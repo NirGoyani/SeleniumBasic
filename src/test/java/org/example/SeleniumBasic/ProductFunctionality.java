56298c9093ccd2ed956813cba7fc207054f50cb0
MojMasala <mojmasala07@gmail.com>
2023-10-14 12:03:35 +0100

    JC-2 Added test cases for Product Functionality

---
+++ src/test/java/org/example/SeleniumBasic/ProductFunctionality.java
@@ -0,0 +1,10 @@
+package org.example.SeleniumBasic;
+
+public class ProductFunctionality {
+    public static void main(String[] args) {
+        //..
+        //..
+        //..
+        //Product automation
+    }
+}
